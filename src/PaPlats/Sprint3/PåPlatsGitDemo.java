b6e09062c41c4c739113e425dbbaa89df68a1829
sigrunolafsdottir <solafsdottir@gmail.com>
2025-10-28 13:23:25 +0100

    creating commit with PPDemo, git branching

---
+++ src/PaPlats/Sprint3/PåPlatsGitDemo.java
@@ -0,0 +1,14 @@
+package PaPlats.Sprint3;
+
+public class PåPlatsGitDemo {
+
+
+    void main(){
+        IO.println("Demoing git1");
+        IO.println("Demoing git2");
+        IO.println("Demoing git3");
+        IO.println("Demoing git4");
+
+    }
+
+}
