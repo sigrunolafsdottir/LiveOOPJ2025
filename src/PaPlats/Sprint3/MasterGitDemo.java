ff09f138d8154b98c1ee327f90d0f7e908d8a1dd
sigrunolafsdottir <solafsdottir@gmail.com>
2025-10-28 14:28:30 +0100

    more conflict in master

--- src/PaPlats/Sprint3/MasterGitDemo.java
+++ src/PaPlats/Sprint3/MasterGitDemo.java
@@ -6,5 +6,7 @@ public class MasterGitDemo {
         IO.println("another printout in master");
         IO.println("Created to induce merge conflict wrinting other stuff");
         IO.println("Created to induce merge conflict conflict conflict conflict");
+
+        IO.println("doing some work in intellij");
     }
 }
